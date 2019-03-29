2c799e2fdb0682f43548657d7fe6bb18fb0d2a1d
CarinaEtec <miguel.sgarbozza@hotmail.com>
2019-03-28 23:21:42 -0300

    Implementacao de modelo

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Carro } from '../modelos/Carro';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
-
+export class HomePage implements OnInit{
+  public carros: Carro[];
+  
+  ngOnInit(): void {
+    this.carros = [
+      {
+        nome: "Fusca",
+        preco: 1000
+      },
+      {
+        nome: "Gol",
+        preco: 2000
+      },
+      {
+        nome: "Ferrari",
+        preco: 2000,
+      },
+      {
+        nome:"teste",
+        preco: 5000
+      },
+      {
+        nome: "Astra",
+        preco: 3000
+      },
+    ]
+  }
 }
