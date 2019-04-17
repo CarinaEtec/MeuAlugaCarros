2ff7adddaae66a08e29f18801a59465e60e00d7a
CarinaEtec <miguel.sgarbozza@hotmail.com>
2019-04-16 23:12:55 -0300

    Aula 16.04

--- src/app/escolha/escolha.page.ts
+++ src/app/escolha/escolha.page.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
 import { ActivatedRoute } from '@angular/router';
 import { Carro } from '../modelos/Carro';
-import { Acessorios } from '../Ac/Acessorios';
+import { Acessorio } from '../modelos/Acessorio';
 
 
 @Component({
@@ -14,7 +14,9 @@ export class EscolhaPage implements OnInit {
 
   private carro: Carro
   
-  private acessorios : Acessorios []
+  private acessorios : Acessorio []
+
+  private precoTotal : number;
 
   constructor(private navCtrl: NavController,
     private activatedRoute: ActivatedRoute) { }
@@ -26,22 +28,30 @@ export class EscolhaPage implements OnInit {
 
         console.log("O carro que chegou na pagina de escolha é: " + this.carro.nome);
       });
+
+      this.precoTotal = this.carro.preco;
       
       this.acessorios = [
-        {
-          "nome": "Freio ABS",  "preco": 800
-        },
-        {
-          "nome": "Ar-Condicionado", "preco": 1000
-        },
-        {
-          "nome": "MP3 Player", "preco": 500
-        },
+        {nome: "Freio ABS", preco:800},
+        {nome: "Ar-Condicionado", preco:1000},
+        {nome: "MP3 Player", preco:500},
       ];
-  }
+    }
 
   voltar(){
     this.navCtrl.back();
+  }
+
+  atualizarTotal(ativo:boolean, acessorio : Acessorio){
+    ativo ? this.precoTotal += acessorio.preco : this.precoTotal -= acessorio.preco;
+
+    /*if (ativo) {
+      this.precoTotal += acessorio.preco;
+    }
+    else {
+      this.precoTotal -= acessorio.preco;
+    }
+*/
   }
 
 }
---
+++ src/app/modelos/Acessorio.ts
@@ -0,0 +1,6 @@
+export interface Acessorio{
+    nome:string
+    preco:number
+}
+/*SÓ TEM PROPRIEDADES, SEM COMPORTAMENTOS
+O SOMAR/SUBTRAIR SERIA EM UMA CLASS VENDAS, JUNTO COM O PRECO TOTAL */
